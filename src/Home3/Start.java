31047caac1b225528b1ec33e84af741771224f0c
smikhun <a.smikhun@gmail.com>
2016-03-16 19:03:10 +0200

    v1.1

---
+++ src/Home3/Start.java
@@ -0,0 +1,45 @@
+package Home3;
+
+/**
+ * Created by User on 15.03.2016.
+ */
+public class Start {
+    public static void main(String[] args) {
+        Group gr1 = new Group(1,"programer");
+
+        Student st1 = new Student("Smikhun", "25", "man", 1, "programmer");
+        Student st2 = new Student("Vovk", "25", "man", 1, "programmer");
+        Student st3 = new Student("Zabolotniy", "25", "man", 1, "programmer");
+        Student st4 = new Student("Smikhun", "25", "man", 1, "programmer");
+        Student st5 = new Student("Bida", "25", "man", 1, "programmer");
+        Student st6 = new Student("Baniy", "25", "man", 1, "programmer");
+        Student st7 = new Student("Goncharenko", "25", "man", 1, "programmer");
+        Student st8 = new Student("Lipkan", "25", "man", 1, "programmer");
+        Student st9 = new Student("Levchenko", "25", "man", 1, "programmer");
+        Student st10 = new Student("Gerasimov", "25", "man", 1, "programmer");
+        Student st11 = new Student("Kustovskiy", "25", "man", 1, "programmer");
+        Student s12 = new Student("Kustovskiy", "25", "man", 1, "programmer");
+
+        st1.printStudent();
+
+        gr1.addStudent(st1);
+        gr1.addStudent(st2);
+        gr1.addStudent(st3);
+        gr1.addStudent(st4);
+        gr1.addStudent(st5);
+        gr1.addStudent(st6);
+        gr1.addStudent(st7);
+        gr1.addStudent(st8);
+        gr1.addStudent(st9);
+        gr1.addStudent(st10);
+        gr1.addStudent(st11);
+
+        /////////////////////
+        gr1.showAllStudents();
+
+        gr1.searchStudents("Smikhun");
+
+    }
+
+
+}
