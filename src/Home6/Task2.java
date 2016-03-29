a16af0a09fe55573a105d10770ec6dcfe89f9ed7
smikhun <a.smikhun@gmail.com>
2016-03-29 21:32:59 +0300

    v1.1

--- src/Home6/Task2.java
+++ src/Home6/Task2.java
@@ -8,7 +8,7 @@ public class Task2 {
     public static void main(String[] args) {
 
         //Create ARRAY
-        int sizeArraySumm = 1000;
+        int sizeArraySumm = 10000000;
         //create Array
         int[] arr = new int[sizeArraySumm];
         for (int i=0; i < arr.length;i++){
@@ -19,34 +19,37 @@ public class Task2 {
 
 
 
-        int sizeTread = 10;                             // numbers thread
+        int sizeTread = 8;                             // numbers thread
         int sizeArrThread = sizeArraySumm/sizeTread;    //size Array in Thread
         ArrThread[] arr1 = new ArrThread[sizeTread];      // create array Threads
 
-        int [] arrThread = new int [sizeArrThread];      //crete array for thread
+        //int [] arrThread = new int [sizeArrThread];      //crete array for thread
         int countStart = 0;
 
         long start = System.nanoTime();
         for (int i = 0; i < arr1.length; i++){ //create and run threads
 
+            int [] arrThread = new int [sizeArrThread];
+
             int countEnd = countStart + sizeArrThread; //diapazon array tread
             int c = 0 ;                                //count fo array thread
-            for (int j = countStart; j < countEnd; i++){ //rebild array
+            for (int j = countStart; j < countEnd; j++){ //rebild array
                 arrThread[c] = arr[j];
                 c++;
             }
             arr1[i] = new ArrThread(sizeArrThread, arrThread);
             arr1[i].start();
         }
-        long threadSum = 0;
+       /* long threadSum = 0;
         for (int i = 0; i < arr.length; i++){
 
-          threadSum = arr1[i].getSum();
+          threadSum += arr1[i].getSum();
         }
+        System.out.println("SUMMA = " + threadSum);      */
 
         long end = System.nanoTime();
         long traceTime = end-start;
-        System.out.println("MiliSec - " + traceTime/1000000);
+        System.out.println("MiliSec = " + traceTime/1000000);
 
     }
 }
